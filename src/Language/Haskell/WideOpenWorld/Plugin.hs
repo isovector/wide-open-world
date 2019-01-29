370a20b0aee326594c3c7f5415de658f9fb4191f
Sandy Maguire <sandy@sandymaguire.me>
2019-01-29 14:58:05 -0500

    why doesnt the evidence show up?

--- src/Language/Haskell/WideOpenWorld/Plugin.hs
+++ src/Language/Haskell/WideOpenWorld/Plugin.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE TemplateHaskellQuotes #-}
 {-# LANGUAGE TupleSections         #-}
+{-# OPTIONS_GHC -Wall         #-}
 
 module Language.Haskell.WideOpenWorld.Plugin
   ( plugin
@@ -7,16 +8,18 @@ module Language.Haskell.WideOpenWorld.Plugin
   ) where
 
 import Control.Arrow (second)
-import Control.Monad
 import Convert (convertToHsDecls)
 import CoreSyn
 import Data.Foldable
+import Data.Traversable
 import DsBinds
 import DsMonad
+import GHC (idType)
 import GHC.WhyArentYouExported
 import IOEnv
-import Language.Haskell.TH hiding (Type, ppr)
-import Language.Haskell.TH.Syntax hiding (Type)
+import InstEnv
+import Language.Haskell.TH hiding (Type, ppr, Kind)
+import Language.Haskell.TH.Syntax hiding (Type, Kind)
 import Language.Haskell.WideOpenWorld.Test
 import OrdList
 import Outputable hiding ((<>))
@@ -25,6 +28,8 @@ import SrcLoc (noSrcSpan)
 import TcEvidence
 import TcPluginM
 import TcRnTypes
+import TcType
+import Type
 import Unsafe.Coerce
 
 newtype IOEnv' env a = IOEnv' (env -> IO a)
@@ -46,14 +51,17 @@ solveJDI :: () -- ^ JDI's TyCon
          -> [Ct]  -- ^ [D]erived constraints
          -> [Ct]  -- ^ [W]anted constraints
          -> TcPluginM TcPluginResult
-solveJDI _ _ _ wanteds = do
+solveJDI _ [] [] wanteds = do
   if (not $ null wanteds)
      then do
         pprTraceM "what" (ppr wanteds)
-        (bs, e) <- unsafeTcPluginTcM $ cool stuff2
+        (bs, e, _, cts) <- unsafeTcPluginTcM $ cool stuff2
+        ctevs <- for cts (newWanted $ ctLoc $ head wanteds)
+        let ctevids = fmap (Var . ctEvEvId) ctevs
         for_ bs setEvBind
-        pure $ TcPluginOk (fmap (e,) wanteds) []
+        pure $ TcPluginOk (fmap (EvExpr $ Var e `mkApps` ctevids,) wanteds) []
      else pure $ TcPluginOk [] []
+solveJDI _ g d w = pprPanic "YO" $ ppr $ g ++ d ++ w
 
 
 localIOEnv :: (g -> g) -> IOEnv (Env g l) a -> IOEnv (Env g l) a
@@ -64,29 +72,33 @@ localIOEnv f m
   . \env -> env { env_gbl = f $ env_gbl env }
 
 
-cool :: [Dec] -> TcM ([EvBind], EvTerm)
+getContext :: Kind -> ([TyVar], [PredType])
+getContext k = (\(ts, a, _) -> (ts, a)) $ tcSplitSigmaTy k
+
+
+cool :: Dec -> TcM ([EvBind], Var, [TyVar], [PredType])
 cool z = localIOEnv clearTcGblEnv $ do
-  let Right m = convertToHsDecls noSrcSpan z
+  let Right m = convertToHsDecls noSrcSpan [z]
   l <- tcRnSrcDecls m
-  void $ pprPanic "hi" $ ppr $ tcg_binds l
   x <- initDsTc $ dsTopLHsBinds $ tcg_binds l
   let binds = drop 1 $ fromOL x
       dict = fst $ head binds
       evBinds = fmap (uncurry mkGivenEvBind . second EvExpr)
               $ binds
-  pure $ (evBinds, EvExpr $ Var dict )
+      (tys, cts) = getContext $ head $ fmap (idType . is_dfun) $ tcg_insts l
+  pure (evBinds, dict, tys, cts)
 
 
-stuff :: [Dec]
+stuff :: Dec
 stuff =
-  [InstanceD Nothing []
+  InstanceD Nothing []
     (AppT (ConT ''Semigroup)
       (ConT ''Int))
-        [FunD '(<>) [Clause [] (NormalB (VarE 'ok)) []]]]
+        [FunD '(<>) [Clause [] (NormalB (VarE 'ok)) []]]
 
-stuff2 :: [Dec]
+stuff2 :: Dec
 stuff2 =
-  [InstanceD Nothing [AppT (ConT ''Num) a] (AppT (ConT ''Num) (AppT ListT a))
-    [ValD (VarP 'fromInteger) (NormalB (InfixE (Just (VarE 'pure)) (VarE '(.)) (Just (VarE 'fromInteger)))) []]]
+  InstanceD Nothing [AppT (ConT ''Num) a] (AppT (ConT ''Num) (AppT ListT a))
+    [ValD (VarP 'fromInteger) (NormalB (InfixE (Just (VarE 'pure)) (VarE '(.)) (Just (VarE 'fromInteger)))) []]
   where a = VarT $ Name (OccName "a") (NameU 0)
 
